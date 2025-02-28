bbcd2e72f45ab67f2b301d7c6e194f8b97b7df16
dmitrikurtukov <kurtukov9@gmail.com>
2025-02-28 17:46:17 +0200

    Initial commit

---
+++ src/main/java/com/flightplanner/FlightPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flightplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPlannerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightPlannerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flightplanner/FlightPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlannerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
